48448b4b099a1de19aa8b3eb953f5c2907d8a068
me-jhonathan <mejhonathan@gmail.com>
2023-02-04 15:46:28 -0800

    fix: cycle issues

--- script.js
+++ script.js
@@ -15,25 +15,20 @@ let cycleTitle = "Work: ";
 // cycle (work/break) counter
 let cycle = 0;
 
-// flow control
-let flowSwitch = false;
-let startCycle = true;
-
 // add leading zero to display numbers
 const zeroPad = (num) => (num < 10) ? '0' + num: num;
 
 // automatic countdown timer
 const countDown = () => {
-    if (flowSwitch == true) {
     cycleBtn.innerHTML = cycle;
     // start work cycle
-    if (cycle % 6 === 0 || cycle % 6 === 2 || cycle % 6 === 4) {
+    if (cycle === 0 || cycle === 2 || cycle === 4) {
       timeLeft = workTime;
       cycleTitle = "Work: "
       startWorkCycleColor();
 
     // start long break cycle
-    } else if (cycle % 6 === 6) {
+    } else if (cycle === 6) {
       timeLeft = longBreakTime;
       cycleTitle = "Long Break: ";
       startLongBreakCycleColor();
@@ -50,7 +45,7 @@ const countDown = () => {
     if (timeLeft <= 0) {
       clearInterval(timer);
       cycle++;
-      if (cycle === 6) {
+      if (cycle === 7) {
         cycle = 0;
       }
       countDown();
@@ -66,50 +61,43 @@ const countDown = () => {
     document.querySelector("#title").innerHTML =
       cycleTitle + zeroPad(minutes) + ":" + zeroPad(seconds);
   }, 1000);
-} 
 };
 
 // start work cycle colors
 function startWorkCycleColor() {
+  startBtn.disabled = true;
   document.body.style.background = "lightcoral";
+  document.body.style.boxShadow = "inset 0 -1vw 4vh 4vh rgba(95, 3, 3, 0.5)";
   playIconStart.style.display = "none";
-
-  // addCycleProperties("startWork");
+  addCycleProperties("startWork");
 }
 
 // start break cycle colors
 function startBreakCycleColor() {
   document.body.style.background = "rgb(104, 152, 223)";
-  // addCycleProperties("startBreak");
+  document.body.style.boxShadow = "inset 0 0 0 0vh rgba(95, 3, 3, 0.5)";
+  addCycleProperties("startBreak");
 }
 
 // start long break cycle colors
 function startLongBreakCycleColor() {
   document.body.style.background = "rgb(192, 128, 235)";
-  // addCycleProperties("startLongBreak");
+  document.body.style.boxShadow = "inset 0 0 0 0vh rgba(95, 3, 3, 0.5)";
+  addCycleProperties("startLongBreak");
 }
 
-// function addCycleProperties(cycleName) {
-//   startBtn.classList = "";
-//   startBtn.classList.add(cycleName);
-// }
+function addCycleProperties(cycleName) {
+  startBtn.classList = "";
+  startBtn.classList.add(cycleName);
+}
 
 // if user clicks on 'start' button start timer
 startBtn.addEventListener("click", (e) => {
-  this.removeEventListener("click", (e) => {});
-  if (flowSwitch == false) {
-    flowSwitch = true;
-    // playArrowIcon.style.visibility = "hidden";
     time.style.visibility = "visible";
-
     startWorkCycleColor();
 
     // start timer
     countDown();
-    // }
-  } else {
-    flowSwitch = false;
-  }
 });
 
 // if user clicks on 'restart' button restart timer and cycle
